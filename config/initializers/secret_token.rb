7d56b1e48d37aab2397fad68576990ce56a7990b
Tyler Mandry <tmandry@gmail.com>
2013-04-15 14:02:43 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapfeed::Application.config.secret_token = 'dfe14b8862d1919a56d5b861fb22cc9c037ddd2e881e7499cceab5fe0c53a9399515cdbefd4143a58b0ca76c604e7349db76d839583cf7c93f77c6c9eed746db'
