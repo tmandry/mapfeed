6d8e2440b3a3b2c2d8b4faaae7635ba11ec1e3a8
Tyler Mandry <tmandry@gmail.com>
2013-04-17 14:58:57 -0500

    Add basic Twitter functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    client = Twitter::Client.new(
+        oauth_token: '14730924-OL77JFECKu31uIXaoJJEYR817MEq3u6GJzGsIa31b',
+        oauth_token_secret: 'RyZBlCDDQo4idFO00asAAsBblzcROkRr4czFCBt1kwY'
+    )
+    @tweets = client.search("to:justinbieber marry me", :count => 3, :result_type => "recent").results
   end
 end
