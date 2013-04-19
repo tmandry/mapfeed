34978768de66e6b4caaa3ab73c36cbcbad3e55a5
Nicholas Hanemann <njmh@tamu.edu>
2013-04-19 14:40:01 -0500

    Pulls tweets from a 2 mile radius around College Station

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,11 @@ class HomeController < ApplicationController
         oauth_token: '14730924-OL77JFECKu31uIXaoJJEYR817MEq3u6GJzGsIa31b',
         oauth_token_secret: 'RyZBlCDDQo4idFO00asAAsBblzcROkRr4czFCBt1kwY'
     )
-    @tweets = client.search("to:justinbieber marry me", :count => 3, :result_type => "recent").results
+    # latitude = 30.6278
+    # longitude = -96.3342
+    # cStat = latitude.to_s + ',' + longitude.to_s + ',2mi'
+
+	@tweets = client.search("-rt -http -@", :geocode => "30.6278,-96.3342,2mi", :count => 8, :result_type => "recent").results
+    # @tweets = client.search("bachata",:count => 8, :result_type => "recent").results
   end
 end
