74cd6e7e7b1d20bca214882c1354d39b5baaf372
Tyler Mandry <tmandry@gmail.com>
2013-05-02 00:25:36 -0500

    Add some actual caching (stays cached for one minute)

--- app/classes/tweet_cache.rb
+++ app/classes/tweet_cache.rb
@@ -1,6 +1,19 @@
 class TweetCache
   def search(query, options = {})
-    client.search(query, options).results
+    # Check for cached results
+    # Some options we do not want to subject our DB query to
+    query_options = options.except(:count, :until, :since_id, :max_id)
+    criteria = TweetSearchResult.where(query: query, options: query_options)
+    cache = criteria.first_or_initialize
+
+    # Update results by hitting the Twitter API, if needed
+    if not cache.tweets or cache.updated_at < 1.minute.ago
+      cache.options = query_options  # not sure why I have to do this.. Mongoid bug?
+      cache.tweets = client.search(query, options).results.collect {|tweet| tweet.to_hash}
+      cache.save
+    end
+
+    cache.tweets.collect {|hash| Twitter::Tweet.new(hash.with_indifferent_access)}
   end
 
   def client
