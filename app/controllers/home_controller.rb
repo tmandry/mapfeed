fcf6bcfe3814d1955ca9b7383b41005dc31b24e0
Tyler Mandry <tmandry@gmail.com>
2013-05-05 15:51:53 -0500

    Make map use query text, get rid of table.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,7 @@
 class HomeController < ApplicationController
-  def index
-    @users = User.all
-    @query = params[:query] rescue " "
-
-    # latitude = 30.6278
-    # longitude = -96.3342
-    # cStat = latitude.to_s + ',' + longitude.to_s + ',2mi'
-
-	@tweets = TweetCache.new.search(@query, :geocode => "30.6278,-96.3342,2mi", :count => 10, :result_type => "recent")
-    # @tweets = client.search("bachata",:count => 8, :result_type => "recent").results
-  end
-
   def tweets
-    @tweets = TweetCache.new.search(@query, :geocode => "30.6278,-96.3342,2mi", :count => 10, :result_type => "recent")
+    query = params[:query] rescue " "
+    @tweets = TweetCache.new.search(query, :geocode => "30.6278,-96.3342,2mi", :count => 10, :result_type => "recent")
     render json: @tweets
   end
 end
