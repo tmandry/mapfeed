f31418c75d75f4491b0947867640a1f992f086f2
Tyler Mandry <tmandry@gmail.com>
2013-05-05 20:39:41 -0500

    Add location option!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,13 @@ class HomeController < ApplicationController
   def tweets
     query = params[:query]
     @radius = (params[:radius] or "2mi")
-    @tweets = TweetCache.new.search(query, :geocode => "30.6278,-96.3342,#{@radius}", :count => 10, :result_type => "recent")
-    render json: @tweets
+    location = (params[:location] or "College Station, TX")
+    geocode = Geocoder.coordinates(location)
+    @tweets = TweetCache.new.search(query, :geocode => "#{geocode[0]},#{geocode[1]},#{@radius}", :count => 100, :result_type => "recent")
+    results = {
+      tweets: @tweets,
+      center: geocode
+    }
+    render json: results
   end
 end
