f70eb364e885b3c0685fdceeb9918a07da9d7f6b
Tyler Mandry <tmandry@gmail.com>
2013-05-05 20:02:49 -0500

    Fix default radius

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def tweets
-    query = params[:query] rescue " "
-    @radius = params[:radius] rescue "2mi"
+    query = params[:query]
+    @radius = (params[:radius] or "2mi")
     @tweets = TweetCache.new.search(query, :geocode => "30.6278,-96.3342,#{@radius}", :count => 10, :result_type => "recent")
     render json: @tweets
   end
